588b66d6dbdea62bc963cfcb75cca5234a2412dc
Nils Böhnke <s4niboeh@uni-trier.de>
2023-11-23 18:17:38 +0100

    Änderung von Farbe blau zu grau

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
+        Car car3 = new Car("gray", "VW", 3);
 
         Car car4 = new Car("yellow", "Renault", 4);
 

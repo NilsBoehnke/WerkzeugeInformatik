9ea51f65d791a370fada983f05707a549738ab60
Nils Böhnke <s4niboeh@uni-trier.de>
2023-11-23 19:23:40 +0100

    Änderung der Farbe blau zu pink

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Factory factory = new Factory();
 
         Car car1 = new Car("red", "BMW", 5);
-        Car car2 = new Car("blue", "Audi", 3);
+        Car car2 = new Car("pink", "Audi", 3);
         Car car3 = new Car("pink", "VW", 3);
 
         factory.buildCar(car1);
